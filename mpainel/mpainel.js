ec22c6c51d7e7a72c046bd1bee712d3430649677
Nuno Figueira <ubuntu_18.04_pc@nundel.com>
2023-01-12 03:50:13 -0800

    Add comment to the javascript functions

--- mpainel/mpainel.js
+++ mpainel/mpainel.js
@@ -1,94 +1,123 @@
-<script>
-  function getNames(item)
-  {
-      let newli   = document.createElement('li');
-      let newSpan = document.createElement('span');
-      let element = document.getElementById('utentes-list');
-      newSpan.innerHTML = `${item.code}`;
-      newli.appendChild(newSpan);
-      element.appendChild(newli);
-  }
-</script>
+/***************************************************************************************************** 
+ * Author   : Nuno Figueira                                                                          *
+ * Dev Year : 2019/2020                                                                              *
+ * Release  : 1.1.0 beta                                                                             *
+ * Project  : Agenda Medic - Mpainel                                                                 *
+ *                                                                                                   *
+ * Javascript functionality:                                                                         *
+ * The main purpose of this javascript code is to manager the info of tickets printed by the         *
+ * terminal machine:                                                                                 *  
+ * The Restfull API is called to GET all tickets, with status waiting, and the ticket will be print  *
+ * on Monitor screen.                                                                                *
+ * But, after the ticket timeout expire, the Resfull API, PUT method, is fetch to updated the fields *
+ * status and datetime. To closed and with current time, respectivily.                               * 
+ * Using a JS Worker we don't need refresh the full Single Page. Just refresh the HTML element list  *
+ * of ticket, when the Worker event is trigger.                                                      *    
+ * **************************************************************************************************/
 
-<script>
-  var listOfCalledTickets = [];
-  var dateTime = new Date();
-
-function clearListOfNames()
-{
-     var list = document.getElementById("utentes-list");
-           while (list.hasChildNodes()) {
-             list.removeChild(list.firstChild);
-         }
+// Function description:
+// Create a dynamic HTML compoment, as type list, to display the ticket printed on the terminal machine.
+// Everytime we print or requeste a new ticket, on the terminal, this function will added an element in the HTML list.
+function getNames(item) {
+  let newli = document.createElement('li');
+  let newSpan = document.createElement('span');
+  let element = document.getElementById('ticket-list');
+  newSpan.innerHTML = `${item.code}`;
+  newli.appendChild(newSpan);
+  element.appendChild(newli);
 }
 
+// Declaeration of global variables:
+var listOfCalledTickets = [];
+var dateTime = new Date();
+
+// Function description:
+// This function remove all the ticked "closed" from the display screen.
+function clearListOfTicketsId() {
+  var list = document.getElementById("ticket-list");
+  //if (list.length > 0)
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
+}
+
+// Function description:
+// Execute the Restfull API <POST> to update the Ticket status to close.
+// The ticket field datetime will be updated too with the current close time.
 function processTicketsCalled(element) {
-// after GET the Tickets from MONGO DB save each unit
-// and update the field status for closed.
-  element.status = "closed";
+  element.status = "close";
   element.closeTime = dateTime.toLocaleDateString();
   listOfCalledTickets.push(element);
 }
+
+// Function description:
+// THIS FUNCTIONS IS CALL EACH 5 MINUTES TO UPDATE THE STATUS OF OPENS TICKETS...
+// After the timer to display the ticket on the monitors expired, the ticket show on 
+// the monitor is removed. And the field status and the datetime, are updated with the 
+// closed and the current time respectivily. To finish the procedure the PUT is executed.
 function putTicketStatusClose() {
-  if(listOfCalledTickets.length >=1) {
+  if (listOfCalledTickets.length >= 1) {
 
-      let urlApi = "";
-      var json={};
-      var calledTicket = {};
-      var xhr  = new XMLHttpRequest();
+    let urlApi = "";
+    var json = {};
+    var calledTicket = {};
+    var xhr = new XMLHttpRequest();
 
-      console.log("ENTROU NO PUT");
-      listOfCalledTickets.forEach( function(ticket){
-         urlApi = 'http://10.10.10.90:5000/registers/'+ ticket._id;
-         calledTicket = {
-            _id: ticket._id,
-            counter: ticket.counter,
-            code: ticket.code,
-            status: ticket.status,
-            serviceType: ticket.serviceType,
-            openTime: ticket.openTime,
-            closeTime: ticket.closeTime
-           };
+    console.log("PROCESSING TICKET ON PUT");
+    listOfCalledTickets.forEach(function (ticket) {
+      urlApi = 'http://10.10.10.90:5000/registers/' + ticket._id;
+      calledTicket = {
+        _id: ticket._id,
+        counter: ticket.counter,
+        code: ticket.code,
+        status: ticket.status,
+        serviceType: ticket.serviceType,
+        openTime: ticket.openTime,
+        closeTime: ticket.closeTime
+      };
 
-       console.log("URL:"+ urlApi);
-       console.log("new status:"+ calledTicket.status);
-       console.log("close time:"+ calledTicket.closeTime);
+      console.log("URL:" + urlApi);
+      console.log("new status:" + calledTicket.status);
+      console.log("close time:" + calledTicket.closeTime);
 
       json = JSON.stringify(calledTicket);
       xhr.open("PUT", urlApi, true);
-      xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+      xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
       xhr.onload = function () {
-      var users = JSON.parse(xhr.responseText);
-      if (xhr.readyState == 4 && xhr.status == "200") {
+        var users = JSON.parse(xhr.responseText);
+        if (xhr.readyState == 4 && xhr.status == "200") {
           console.table(users);
-          listOfCalledTicket=[];
-          }
-     else {
-              console.error(users);
-    }
-   }
-   xhr.send(json);
+          listOfCalledTickets = [];
+        }
+        else {
+          console.error(users);
+        }
+      }
+      xhr.send(json);
 
-});
-}}
-</script>
+    });
+  }
+}
 
-<script>
-  /*THIS IS THE WORKER THAT WILL UPDATE THE USER DATA IN BACKGROUND*/
-  function updateInfo() {
-    const worker = new Worker('refreshPainelUserInfo.js')
-    worker.onmessage = function(event) {
-       putTicketStatusClose();
-       clearListOfNames();
-       const url='http://10.10.10.90:5000/registers/terminal';
-          fetch(url)
-           .then(response => response.json())
-           .then(data => {
-             data.message.forEach(getNames);
-             data.message.forEach(processTicketsCalled);
-          })
-       .catch(error => alert(error));
-   };
- }
-</script>
-
+// Function description:
+// The main algorithm will be executed under a Javascript WORKER procedure.
+// In each event of the Worker, the followings methods are will be executed:
+// putTicketStatusClose () -> update the ticket fields, status and datetime.
+// clearListOfTicketsId () -> stop display the expiered ticket on the monitor. 
+// getNames ()             -> Get the ID of the ticket to display on the monitor.
+// processTicketsCalled () -> save the "PUT" the ticket with the fields updated. 
+function updateInfo() {
+  const worker = new Worker('refreshPainelUserInfo.js')
+  worker.onmessage = function (event) {
+    putTicketStatusClose();
+    clearListOfTicketsId();
+    const url = 'http://10.10.10.90:5000/registers/terminal';
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        data.message.forEach(getNames);
+        //data.message.forEach(processTicketsCalled);
+      })
+      .catch(error => alert(error));
+  };
+}
