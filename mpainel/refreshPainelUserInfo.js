2b857022f0ea04f8b94b11879d0a8b2a64d5bd1d
Nuno Figueira <ubuntu_18.04_pc@nundel.com>
2023-01-12 07:22:14 -0800

    Changed the name of i variable to expireTimer, just to be more understood with the meaning we are use this variable.

--- mpainel/refreshPainelUserInfo.js
+++ mpainel/refreshPainelUserInfo.js
@@ -1,6 +1,21 @@
-let i=1;
-setInterval(mult,150000);
-function mult(){
-  i= i* 2;
-  postMessage(i);
+/****************************************************************************************************** 
+ * Author   : Nuno Figueira                                                                           *
+ * Dev Year : 2019/2020                                                                               *
+ * Release  : 1.1.0 beta                                                                              *
+ * Project  : Agenda Medic - Mpainel                                                                  *
+ *                                                                                                    *
+ * Javascript worker functionality:                                                                   *
+ * The worker is not full developed in this release. For now its basically used as a timer for now.   *
+ * to manager the fetch interval used to refresh the list o Tickets ID to display.                    *
+ *                                                                                                    *
+ * But the point is:                                                                                  *
+ * In future release, we pretend use a JS worker to execute the Fecth API data actions. Avoid doing   *
+ * this operation on fronted side.                                                                    *
+ * ****************************************************************************************************/
+
+let expireTimer = 1;
+setInterval(mult, 150000);
+function mult() {
+  expireTimer = expireTimer * 2;
+  postMessage(expireTimer);
 }
