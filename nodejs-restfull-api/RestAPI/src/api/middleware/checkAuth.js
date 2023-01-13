5ef90c3d3a05a1314aacef3b62b88b559a13289c
root <EngNunoFigueira@gmail.com>
2023-01-13 05:58:25 -0800

    token validation used in middleware

--- nodejs-restfull-api/RestAPI/src/api/middleware/checkAuth.js
+++ nodejs-restfull-api/RestAPI/src/api/middleware/checkAuth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
-        const token=req.body.token;
+        const token = req.body.token;
         const decoded = jwt.verify(token, process.env.JWT_KEY);
         next();
     } catch (error) {
