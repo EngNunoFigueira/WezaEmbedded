16a29dc7976d6a98c44692caf21bc3229ca31e55
Nuno Figueira <ekumbi48172@hotmail.com>
2024-04-09 06:38:53 -0700

    Updated the code example how we implement the design parttern observer in Cpp

--- CppLesson/code-challenges/design-pattern-visitor/main.cpp
+++ CppLesson/code-challenges/design-pattern-visitor/main.cpp
@@ -1,28 +1,27 @@
-#include <iostream>
-#include <stdio.h>
-#include <map>
-#include <vector>
-#include <thread>
-#include <future>
-#include <utility>
-#include <memory>
-#include <iterator>
-#include <numeric>
 #include <algorithm>
-#include <unordered_map>
-#include <functional>
-#include <initializer_list>
-#include <cstddef>
 #include <cerrno>
 #include <cfenv>
 #include <cmath>
-#include <fstream>
+#include <cstddef>
 #include <cstring>
+#include <fstream>
+#include <functional>
+#include <future>
+#include <initializer_list>
 #include <iomanip>
+#include <iostream>
+#include <iterator>
 #include <list>
-#include <sstream>
+#include <map>
+#include <memory>
+#include <numeric>
 #include <regex>
-
+#include <sstream>
+#include <stdio.h>
+#include <thread>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
 class Licor;
 class Tobacco;
@@ -31,109 +30,104 @@ class Necessity;
 class Visitor {
 
 public:
-    virtual double visit(const Licor *licor) const = 0;
-    virtual double visit(const Tobacco *tobacco) const = 0;
-    virtual double visit(const Necessity *necessity) const = 0;
+  virtual double visit(const Licor *licor) const = 0;
+  virtual double visit(const Tobacco *tobacco) const = 0;
+  virtual double visit(const Necessity *necessity) const = 0;
 };
 
-class Visitable
-{
+class Visitable {
 public:
-
-    virtual ~Visitable() {};
-    virtual double accept(Visitor *visitor) const = 0;
-
+  virtual ~Visitable(){};
+  virtual double accept(Visitor *visitor) const = 0;
 };
 
-/*Now we implement the these classes used in the VISITOR INTERFACE*/
+//Now we implement the these classes used in the VISITOR INTERFACE
 
-class Licor : public Visitable
-{
+class Licor : public Visitable {
+private:
+  double productPrice{};
+
 public:
-    Licor(double price):
-        productPrice(price) {}
-    double getPrice() const {
-        return productPrice;
-    }
-    virtual double accept(Visitor *visitor) const override {
-        return visitor->visit(this);
-    }
+  Licor(double price) : productPrice(price) {}
+  double getPrice() const { return productPrice; }
+  virtual double accept(Visitor *visitor) const override {
+    return visitor->visit(this);
+  }
+};
+
+class Tobacco : public Visitable {
+public:
+  Tobacco(double price) : productPrice(price) {}
+  double getPrice() const { return productPrice; }
+  virtual double accept(Visitor *visitor) const override {
+    return visitor->visit(this);
+  }
 
 private:
-    double productPrice {};
+  double productPrice{};
 };
 
-class Tobacco : public Visitable
-{
+class Necessity : public Visitable {
 public:
-    Tobacco(double price):
-        productPrice(price) {}
-    double getPrice() const {
-        return productPrice;
-    }
-    virtual double accept(Visitor * visitor) const override {
-        return visitor->visit(this);
-    }
+  Necessity(double price) : productPrice(price) {}
+  double getPrice() const { return productPrice; }
+  virtual double accept(Visitor *visitor) const override {
+    return visitor->visit(this);
+  }
+
 private:
-    double productPrice {};
+  double productPrice{};
 };
 
-class Necessity : public Visitable
-{
+/*
+ * This is the class that will consume the resource of the design pattern
+ * visitor
+ **/
+
+class ConsumerVisitor : public Visitor {
 public:
-    Necessity(double price):
-        productPrice(price) {}
-    double getPrice() const { 
-        return productPrice; 
-    }
-    virtual double accept(Visitor *visitor) const override {
-        return visitor->visit(this);
-    }
-private:
-    double productPrice {};
+  virtual double visit(const Licor *licor) const override {
+    std::cout << "The licor price is " << licor->getPrice() << std::endl;
+    return licor->getPrice();
+  }
+  virtual double visit(const Tobacco *tobacco) const override {
+    std::cout << "The tobacco price is " << tobacco->getPrice() << std::endl;
+    return tobacco->getPrice();
+  }
+  virtual double visit(const Necessity *necessity) const override {
+    std::cout << "The clothes price is " << necessity->getPrice() << std::endl;
+    return necessity->getPrice();
+  }
 };
 
-/*----- This is the class that will consume the resource of the design pattern visitor----*/
+int main() {
+  std::cout << "This exercise show as the design pattern visitor is implemented"
+            << std::endl;
+  std::cout << "What the Visitor design pattern can do for us." << std::endl;
+  std::cout << "FIRST FEATURE:" << std::endl;
+  std::cout << "Allow you to add methods to classes of different types without "
+               "much altering those classes."
+            << std::endl;
+  std::cout << "SECOND FEATURE:" << std::endl;
+  std::cout
+      << "You can make completely different methods depending on the class used"
+      << std::endl;
+  std::cout << "SECOND THIRD:" << std::endl;
+  std::cout << "Allow you to define external classes that can extend other "
+               "classes without majorly editing them"
+            << std::endl;
 
-class ConsumerVisitor : public Visitor
-{
-public:
-    virtual double visit(const Licor *licor) const override {
-        std::cout << "The licor price is " << licor->getPrice() << std::endl;
-        return licor->getPrice();
-    }
-    virtual double visit(const Tobacco *tobacco) const override {
-        std::cout << "The tobacco price is " << tobacco->getPrice() << std::endl;
-        return tobacco->getPrice();
-    }
-    virtual double visit(const Necessity *necessity) const override {
-        std::cout << "The clothes price is " << necessity->getPrice() << std::endl;
-        return necessity->getPrice();
-    }
-};
+  Licor *limao = new Licor(34.99);
+  Tobacco *garret = new Tobacco(12.30);
+  Necessity *cloths = new Necessity(745.59);
 
+  ConsumerVisitor typeProductLicor;
+  ConsumerVisitor *reverse = new ConsumerVisitor();
+  typeProductLicor.visit(limao);
+  typeProductLicor.visit(cloths);
 
-int main()
-{
-    std::cout << "This exercise show as the design pattern visitor is implemented" << std::endl;
-    std::cout << "What the Visitor design pattern can do for us." << std::endl;
-    std::cout << "FIRST FEATURE:" << std::endl;
-    std::cout << "Allow you to add methods to classes of different types without much altering those classes." << std::endl;
-    std::cout << "SECOND FEATURE:" << std::endl;
-    std::cout << "You can make completely different methods depending on the class used" << std::endl;
-    std::cout << "SECOND THIRD:" << std::endl;
-    std::cout << "Allow you to define external classes that can extend other classes without majorly editing them" << std::endl;
+  std::cout << "REVERSE NOW THE VISITOR:" << std::endl;
+  cloths->accept(reverse);
 
-    Licor     *limao  = new Licor(34.99);
-    Tobacco   *garret = new Tobacco(12.30);
-    Necessity *cloths = new Necessity(745.59);
-    ConsumerVisitor typeProductLicor;
-    ConsumerVisitor *reverse = new ConsumerVisitor();
-    typeProductLicor.visit(limao);
-    typeProductLicor.visit(cloths);
-
-    std::cout << "REVERSE NOW THE VISITOR:" << std::endl;
-   cloths->accept(reverse);
-
-    return 0;
+  return 0;
 }
