44346a9cc6275ee3a6bff6d586ee6e913916e156
Nuno Figueira <ekumbi48172@hotmail.com>
2024-09-20 04:57:25 -0700

    Now this code are avaible on the repository of github

---
+++ CppLesson/code-challenges/design-parttern.singlethon/main.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <cerrno>
+#include <cfenv>
+#include <cmath>
+#include <cstddef>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <future>
+#include <initializer_list>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <regex>
+#include <sstream>
+#include <stdio.h>
+#include <thread>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+
+
+int main()
+{
+
+	return 0;
+}
